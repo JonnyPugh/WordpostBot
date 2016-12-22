ab2c15fd7f56c3ec49d42fba0f768536d8cef31f
JonnyPugh <jpugh@umich.edu>
2016-12-22 07:58:36 -0500

    Comments with definitions are now posted on plural word posts.

--- Project/make_post.py
+++ Project/make_post.py
@@ -5,6 +5,7 @@ from MySQLdb import connect
 from MySQLdb.cursors import DictCursor
 from math import ceil
 from sets import Set
+from re import match
 import requests
 import time
 import os
@@ -42,6 +43,16 @@ def get_wordnik_json(route, extra_params):
 		request_json = request.json()
 	return request_json
 
+def post_word(route, word):
+	definition = get_wordnik_json("word.json/"+word+"/definitions", {})[0]
+	data = {
+		"message": word+(" - "+definition["partOfSpeech"] if "partOfSpeech" in definition else "")+"\n"+definition["text"],
+		"access_token": page_info["access_token"]
+	}
+	r = requests.post("https://graph.facebook.com/v2.8/"+route, data=data)
+	r.raise_for_status()
+	return r.json()["id"], definition["text"]
+
 def write_to_log(log_file, message):
 	log_file.write("["+time.ctime()+"]:\t"+message+"\n")
 
@@ -54,25 +65,27 @@ def main():
 	# Form a set of all posted words to prevent reposts
 	posted_words = Set([post["word"] for post in execute_query("select word from Posts")])
 
-	# Get word data to post
+	# Get word to post
 	while True:
 		word = get_wordnik_json("words.json/randomWords", {"minLength": 0})[0]["word"]
 		if word in posted_words:
 			write_to_log(error_log, "Word: '"+word+"' already posted, posting another...")
 			continue
 		break
-	definition = get_wordnik_json("word.json/"+word+"/definitions", {})[0]
 
 	# Make a post, insert its data into the database, and log it
-	data = {
-		"message": word+(" - "+definition["partOfSpeech"] if "partOfSpeech" in definition else "")+"\n"+definition["text"],
-		"access_token": page_info["access_token"]
-	}
-	r = requests.post("https://graph.facebook.com/v2.8/"+page_info["page_id"]+"/feed", data=data)
-	r.raise_for_status()
-	execute_query("insert into Posts values (%s, %s, %s)", (int(ceil(time.time())), r.json()["id"], word))
+	post_id, definition = post_word(page_info["page_id"]+"/feed", word)
+	execute_query("insert into Posts values (%s, %s, %s)", (int(ceil(time.time())), post_id, word))
 	with open("posts.log", "a+") as posts_log:
 		write_to_log(posts_log, "Finished posting word - "+word)
+
+	# If the posted word was plural, post the 
+	# definition of the root word as a comment
+	plural_word = match("Plural form of (.*?)[.]", definition)
+	if plural_word:
+		root_word = plural_word.group(1)
+		post_word(post_id+"/comments", root_word)
+		write_to_log(posts_log, "Posted comment definition of word '"+root_word+"' on post with definition of '"+word"'")
 
 if __name__ == "__main__":
 	try:
